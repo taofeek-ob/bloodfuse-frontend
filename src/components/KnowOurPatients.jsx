42717b4690f3e8eddf4f884999f807056fbc5102
taofeek-ob <taofeek01@yahoo.com>
2022-07-31 13:19:55 +0100

    video slider and navigation button responsiveness

--- src/components/KnowOurPatients.jsx
+++ src/components/KnowOurPatients.jsx
@@ -1,50 +1,62 @@
-import VideoCard from './VideoCard'
+import VideoCard from "./VideoCard";
 import { Swiper, SwiperSlide } from "swiper/react";
 import { Pagination, Navigation } from "swiper";
 import "swiper/css";
 import "swiper/css/pagination";
 import "swiper/css/navigation";
-
+import { useState } from "react";
 
 const KnowOurPatients = () => {
-    const persons = [
-        {
-            name: "Oluwole Seun",
-            videoSrc: "",
-            details: "Meet 14 years old Seun from Lagos. She and others who are fighting Leukemia often need blood trans-fusions while going through chemo treatments.",
-            btnDescription: "Watch on Our Youtube",
-            youtubeLink: "www.youtube.com"
-        },
-        {
-            name: "Audu Haruna",
-            videoSrc: "",
-            details: "Meet 28 years old Haruna from Kano, who is fighting Cancer often need blood trans-fusions while going through chemo treatments.",
-            btnDescription: "Watch on Our Youtube",
-            youtubeLink: "www.youtube.com"
-        }
-    ]
-    return (
-        <section className='w-full pb-8 md:pb-16 h-auto'>
-            <h1 className="text-2xl md:text-3xl text-center py-6 px-2 font-bold hidden sm:block">GET TO KNOW OUR PATIENTS MORE</h1>
-            <h1 className="text-2xl md:text-3xl text-center py-6 px-2 font-bold block sm:hidden">GET TO KNOW OUR PATIENTS</h1>
-            {/* The Swiper Component */}
-            <Swiper
-                slidesPerView={1}
-                spaceBetween={30}
-                loop={true}
-                navigation={true}
-                modules={[Pagination, Navigation]}
-                className="mySwiper w-full"
-            >
-                {persons.map((person, index) => (
-                    <SwiperSlide key={index}>
-                        <VideoCard person={person} />
-                    </SwiperSlide>
-                ))}
+  const screenWidth = () => {
+    if (window.innerWidth < 768) {
+      return false;
+    } else {
+      return true;
+    }
+  };
+  const persons = [
+    {
+      name: "Oluwole Seun",
+      videoSrc: "",
+      details:
+        "Meet 14 years old Seun from Lagos. She and others who are fighting Leukemia often need blood trans-fusions while going through chemo treatments.",
+      btnDescription: "Watch on Our Youtube",
+      youtubeLink: "https://www.youtube.com/embed/JU5ZoGU3wzA",
+    },
+    {
+      name: "Audu Haruna",
+      videoSrc: "",
+      details:
+        "Meet 28 years old Haruna from Kano, who is fighting Cancer often need blood trans-fusions while going through chemo treatments.",
+      btnDescription: "Watch on Our Youtube",
+      youtubeLink: "https://www.youtube.com/embed/fehAgOqTR44",
+    },
+  ];
+  return (
+    <section className="w-full pb-8 md:pb-16 h-auto">
+      <h1 className="text-2xl md:text-3xl text-center py-6 px-2 font-bold hidden sm:block">
+        GET TO KNOW OUR PATIENTS MORE
+      </h1>
+      <h1 className="text-2xl md:text-3xl text-center py-6 px-2 font-bold block sm:hidden">
+        GET TO KNOW OUR PATIENTS
+      </h1>
+      {/* The Swiper Component */}
+      <Swiper
+        slidesPerView={1}
+        spaceBetween={30}
+        loop={true}
+        navigation={screenWidth()}
+        modules={[Pagination, Navigation]}
+        className="mySwiper w-full"
+      >
+        {persons.map((person, index) => (
+          <SwiperSlide key={index}>
+            <VideoCard person={person} />
+          </SwiperSlide>
+        ))}
+      </Swiper>
+    </section>
+  );
+};
 
-            </Swiper>
-        </section>
-    )
-}
-
-export default KnowOurPatients
+export default KnowOurPatients;
--- src/components/VideoCard.jsx
+++ src/components/VideoCard.jsx
@@ -1,29 +1,45 @@
-import { ChevronRightIcon } from '@heroicons/react/solid'
-import AboutWoman from '../assets/about-woman.png';
+import { ChevronRightIcon } from "@heroicons/react/solid";
+import AboutWoman from "../assets/about-woman.png";
 
-const VideoCard = ({ person}) => {
-    // {
-    //     name: "Oluwole Seun",
-    //     videoSrc: "",
-    //     details: "Meet 14 years old Seun from Lagos. She and others who are fighting Leukemia often need blood trans-fusions while going through chemo treatments.",
-    //     btnDescription: "Watch Seun's story on Our Youtube",
-    //     youtubeLink: "youtube.com"
-    // }
+const VideoCard = ({ person }) => {
+  // {
+  //     name: "Oluwole Seun",
+  //     videoSrc: "",
+  //     details: "Meet 14 years old Seun from Lagos. She and others who are fighting Leukemia often need blood trans-fusions while going through chemo treatments.",
+  //     btnDescription: "Watch Seun's story on Our Youtube",
+  //     youtubeLink: "youtube.com"
+  // }
   return (
-    <div className='px-8 flex flex-col'>
-        <div className='w-[80%] mx-auto'>
-        <img src={AboutWoman} alt=" w-full h-full object-cover object-center" />
-        </div>
-      <p className='text-center font-medium py-4 text-2xl lg:text-3xl'>{person.name}</p>
-      <p className='text-center font-[100] pb-6 text-[14px] lg:text-base'>{person.details}</p>
-      <div className='w-full py-2 mx-auto md:w-[60%] flex justify-center'>
-        <a href={person.youtubeLink} className="bttn bttn-primary flex items-center gap-2">
-            <span className='text-[14px] lg:text-base'>{person.btnDescription}</span>
-            <ChevronRightIcon className='h-5 w-5'/>
-            </a>
+    <div className="px-8 flex flex-col">
+      <div className="w-[80%] mx-auto">
+        <iframe
+          className="w-full md:aspect-[20/9] aspect-video mx-auto px-auto md:px-20"
+          src={person.youtubeLink}
+          title="YouTube video player"
+          frameborder="0"
+          allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+          allowfullscreen
+        ></iframe>
+      </div>
+      <p className="text-center font-medium py-4 text-2xl lg:text-3xl">
+        {person.name}
+      </p>
+      <p className="text-center font-[100] pb-6 text-[14px] lg:text-base">
+        {person.details}
+      </p>
+      <div className="w-full py-2 mx-auto md:w-[60%] flex justify-center">
+        <a
+          href={person.youtubeLink}
+          className="bttn bttn-primary flex items-center gap-2"
+        >
+          <span className="text-[14px] lg:text-base">
+            {person.btnDescription}
+          </span>
+          <ChevronRightIcon className="h-5 w-5" />
+        </a>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default VideoCard
+export default VideoCard;
