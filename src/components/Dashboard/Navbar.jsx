11ba0f7562b0aa73f5a1c73de5a800fa7698ffb5
Frank Udoags <frankudoags@gmail.com>
2022-08-08 18:54:22 +0100

    Dashboard sidebar and navbar works

--- src/components/Dashboard/Navbar.jsx
+++ src/components/Dashboard/Navbar.jsx
@@ -1,10 +1,33 @@
 import React from "react";
+import { BellIcon } from "@heroicons/react/outline";
+import ProfilePhoto from '../../assets/profile-photo.jpg';
+import { ChevronDownIcon } from "@heroicons/react/solid";
+import DropletIcon from '../../assets/logo-droplet.png';
+import { MenuIcon } from "@heroicons/react/outline";
 
 const Navbar = () => {
   return (
-    <div className="flex items-center w-full justify-between px-10 py-5 bg-blue-200">
-      <div className="float-left">Dashboard</div>
-      <div className="">Connect STX Account</div>
+    <div className="flex items-center w-full justify-between px-6 py-5 bg-[#FCFCFC]">
+      <div className="flex lg:hidden gap-4 items-center">
+        <img className="h-8 w-auto" src={DropletIcon} alt="Logo" />
+        <MenuIcon className="h-8 w-8" />
+      </div>
+      <div className="hidden lg:block text-2xl">Dashboard</div>
+      <div className="flex items-center gap-6">
+        <button className="hidden md:block bg-[#FFF5F7] py-2 px-4 rounded-full text-[#F00530] border border-transparent focus:border-[#F00530]">Connect STX Account</button>
+        {/* Notifications Icon */}
+        <div><BellIcon className="text-black h-8 w-8 cursor-pointer" /></div>
+        {/* Profile Icon */}
+        <div className="flex items-center gap-2 cursor-pointer">
+          <div className="h-8 w-8 rounded-full overflow-hidden relative">
+            <img className="absolute h-full w-full object-center object-cover" src={ProfilePhoto} alt="menu" />
+          </div>
+          <div className="hidden md:flex items-center gap-2 cursor-pointer">
+          <div className="text-black">OluwaTobi Timothy</div>
+          <ChevronDownIcon className="text-black h-6 w-6" />
+         </div>
+        </div>
+      </div>
     </div>
   );
 };
--- src/components/Dashboard/SideBar.jsx
+++ src/components/Dashboard/SideBar.jsx
@@ -1,13 +1,45 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import Logo from "../../assets/logo-dark.png";
+import { RiHistoryLine } from "react-icons/ri";
+import {MdOutlineSpaceDashboard} from 'react-icons/md';
+import { BsWallet } from "react-icons/bs";
+import { TbHeartbeat } from "react-icons/tb";
+import Logo  from "../../assets/logo-dark.png";
 
 const SideBar = () => {
   return (
-    <div className="flex flex-col items-start gap-4 px-4 bg-red-200 h-full">
-      <Link to="/">
+    <div className="hidden md:flex flex-col items-start gap-4 h-full">
+      <Link to="/" className="py-5 px-4">
         <img className="h-10 w-auto" src={Logo} alt="Logo" />
       </Link>
+
+      
+      <div className="flex flex-col items-start gap-y-6">
+      <Link to="/dashboard" className="text-black py-2 px-4">
+        <div className="flex items-center gap-x-3">
+        <MdOutlineSpaceDashboard className="h-6 w-6 ml-2" />
+        <p>Dashboard</p>
+        </div>
+      </Link>
+      <Link to="/dashboard" className="text-black py-2 px-4">
+        <div className="flex items-center gap-x-3">
+        <TbHeartbeat className="h-6 w-6 ml-2" />
+        <p>Medicals</p>
+        </div>
+      </Link>
+      <Link to="/dashboard" className="text-black py-2 px-4">
+        <div className="flex items-center gap-x-3">
+        <BsWallet className="h-6 w-6 ml-2" />
+        <p>Wallet</p>
+        </div>
+      </Link>
+      <Link to="/dashboard" className="text-black py-2 px-4">
+        <div className="flex items-center gap-x-3">
+        <RiHistoryLine className="h-6 w-6 ml-2" />
+        <p>History</p>
+        </div>
+      </Link>
+      </div>
     </div>
   );
 };
