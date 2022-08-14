36d91c1f8a846a9ddad65ad860897fe4c23127f5
Frank Udoags <frankudoags@gmail.com>
2022-08-14 15:27:10 +0100

    Fixed major needed changes

--- src/components/Nav/Navbar.jsx
+++ src/components/Nav/Navbar.jsx
@@ -215,12 +215,18 @@ export default function NavBar({ bgColor, textColor }) {
             {loginState && (
               <Popover className="relative">
                 {({ open }) => (
+                  <>
+                  <Popover.Button
+                  className={classNames(
+                    open ? "text-[#F00530]" : `tex-${textColor}`,
+                    "group bg-transparent outline-none inline-flex items-center text-base hover:text-white-900"
+                  )}
+                >
                   <div
                     className="hidden md:flex items-center gap-2 cursor-pointer"
-                    onClick={handleLogout}
                   >
                     <div
-                      className={`h-8 w-8 rounded-full overflow-hidden relative border-2 border-${textColor}`}
+                      className={`h-6 w-6 rounded-full overflow-hidden relative border border-${textColor}`}
                     >
                       <img
                         className="absolute h-full w-full object-center object-cover"
@@ -228,11 +234,43 @@ export default function NavBar({ bgColor, textColor }) {
                         alt="menu"
                       />
                     </div>
-                    <div className={`hidden md:flex text-${textColor}`}>
+                    <div className={`hidden md:flex trans`}>
                       {username}
                     </div>
-                    <ChevronDownIcon className={`text-${textColor} h-6 w-6`} />
+                    <ChevronDownIcon
+                        className={classNames(
+                          open
+                            ? "rotate-180 transform"
+                            : " ",
+                          "h-5 w-5 transition-all duration-200 ease-in-out"
+                        )}
+                        aria-hidden="true"
+                      />
                   </div>
+                  </Popover.Button>
+                   <Transition
+                   as={Fragment}
+                   enter="transition ease-out duration-200"
+                   enterFrom="opacity-0 translate-y-1"
+                   enterTo="opacity-100 translate-y-0"
+                   leave="transition ease-in duration-150"
+                   leaveFrom="opacity-100 translate-y-0"
+                   leaveTo="opacity-0 translate-y-1"
+                 >
+                   <Popover.Panel className="absolute z-20 left-1/2 transform -translate-x-1/2 mt-3 px-2 w-full max-w-md sm:px-0">
+                     <div className="rounded-sm shadow-lg ring-1 ring-black ring-opacity-5 overflow-hidden">
+                       <div className="relative grid gap-6 bg-white px-2 py-6 sm:gap-8">
+                          <Link to="/dashboard/main">
+                         <div className="text-[12px] lg:text-base text-gray-900">Dashboard</div>
+                          </Link>
+                         <div className="text-[12px] lg:text-base text-gray-900" 
+                         onClick={handleLogout}
+                         >Sign out</div>
+                       </div>
+                     </div>
+                   </Popover.Panel>
+                 </Transition>
+                 </>
                 )}
               </Popover>
             )}
