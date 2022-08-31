241c9bf71221f2c3115d90c9f7733d19f05ede82
taofeek-ob <taofeek01@yahoo.com>
2022-08-31 07:16:33 +0100

    Implemented the create medical report modal

--- src/components/Modal/MedicalInfo.jsx
+++ src/components/Modal/MedicalInfo.jsx
@@ -20,7 +20,7 @@ export default function MedicalInfo({ isModalOpen, closeModalFunc }) {
             <div className="fixed inset-0 bg-black bg-opacity-25" />
           </Transition.Child>
 
-          <div className="fixed inset-0 overflow-y-auto">
+          <div className="fixed inset-0 overflow-y-auto ">
             <div className="flex h-auto sm:min-h-full items-center justify-center md:p-4 text-center ">
               <Transition.Child
                 as={Fragment}
@@ -31,7 +31,7 @@ export default function MedicalInfo({ isModalOpen, closeModalFunc }) {
                 leaveFrom="opacity-100 scale-100"
                 leaveTo="opacity-0 scale-95"
               >
-                <Dialog.Panel className="w-full sm:w-[80%] sm:mx-auto h-full sm:h-auto px-5 md:px-16 max-w-2xl transform overflow-hidden sm:rounded-[4px] py-2 bg-white  text-left align-middle shadow-xl">
+                <Dialog.Panel className="w-full sm:w-[80%] sm:mx-auto h-full sm:h-auto px-5 md:px-16 max-w-2xl transform overflow-hidden sm:rounded-[4px] py-2 bg-[#FCFCFC]  text-left align-middle shadow-xl">
                   <Dialog.Title
                     as="h2"
                     className=" w-full my-5  flex md:text-3xl text-xl justify-between font-extrabold text-gray-900"
@@ -54,36 +54,89 @@ export default function MedicalInfo({ isModalOpen, closeModalFunc }) {
                   </p>
                   <div className="flex flex-col gap-2 items-center h-full py-8 px-6 md:px-8">
                     <form className="flex flex-col gap-4 items-center w-full">
-                      <input
-                        className="appearance-none rounded w-full p-4 bg-[#F2F2F2] placeholder-[#BFBFBF] mb-3 leading-tight focus:outline-none focus:shadow-outline border-none focus:ring-0"
-                        type="text"
-                        placeholder="Full Name"
-                        required
-                      />
-                      <input
-                        className="appearance-none rounded w-full p-4 bg-[#F2F2F2] placeholder-[#BFBFBF] mb-3 leading-tight focus:outline-none focus:shadow-outline border-none focus:ring-0"
-                        type="tel"
-                        pattern="[0-9]{3}-[0-9]{3}-[0-9]{4}"
-                        placeholder="Phone Number"
-                        required
-                      />
+                      <div className="grid grid-cols-2 justify-between gap-4 w-full">
+                        <label>First Name</label>
+                        <label>Last Name</label>
+                        <input
+                          className="appearance-none bg-transparent rounded w-full p-4  placeholder-[#BFBFBF]  mb-3 leading-tight focus:outline-none focus:shadow-outline border-1 border-[#BFBFBF] focus:ring-0"
+                          type="text"
+                          placeholder="First Name"
+                          required
+                        />
+                        <input
+                          className="appearance-none bg-transparent rounded w-full p-4  placeholder-[#BFBFBF] mb-3 leading-tight  focus:shadow-outline border-1 border-[#BFBFBF] focus:ring-0"
+                          type="text"
+                          placeholder="Last Name"
+                          required
+                        />
+                      </div>
+                      <div className="grid grid-cols-2 justify-between gap-4 w-full">
+                        <label>Blood Group</label>
+                        <label>State</label>
+                        <select
+                          placeholder="Blood Type"
+                          className=" p-4 bg-transparent  text-[#BFBFBF]  rounded focus:outline-none focus:shadow-outline border-1 border-[#BFBFBF] focus:ring-0"
+                        >
+                          <option value="A+">A+</option>
+                          <option value="A-">A-</option>
+                          <option value="B+">B+</option>
+                          <option value="B-">B-</option>
+                          <option value="AB+">AB+</option>
+                          <option value="AB-">AB-</option>
+                          <option value="O+">O+</option>
+                          <option value="O-">O-</option>
+                        </select>
 
-                      <input
-                        className="appearance-none rounded w-full p-4 bg-[#F2F2F2] placeholder-[#BFBFBF] mb-3 leading-tight focus:outline-none focus:shadow-outline border-none focus:ring-0"
-                        type="date"
-                        pattern="[0-9]{3}-[0-9]{3}-[0-9]{4}"
-                        placeholder="MM/DD/YY"
-                        required
-                      />
-                      <input
-                        className="appearance-none rounded w-full p-4 bg-[#F2F2F2] placeholder-[#BFBFBF] mb-3 leading-tight focus:outline-none focus:shadow-outline border-none focus:ring-0"
-                        type="time"
-                        placeholder="00:00"
-                        required
-                      />
+                        <select className=" p-4 bg-transparent text-[#BFBFBF]  rounded focus:outline-none focus:shadow-outline border-1 border-[#BFBFBF] focus:ring-0">
+                          <option value="Lagos">Lagos</option>
+                          <option value="Ogun">Ogun</option>
+                          <option value="Kaduna">Kaduna</option>
+                          <option value="Anambra">Anambra</option>
+                        </select>
+                      </div>
+                      <div className="grid grid-cols-2 justify-between gap-4 w-full">
+                        <label>Blood Pressure</label>
+                        <label>Blood Sugar</label>
+                        <select
+                          placeholder="Blood Type"
+                          className=" p-4 bg-transparent  text-[#BFBFBF]  rounded focus:outline-none focus:shadow-outline border-1 border-[#BFBFBF] focus:ring-0"
+                        >
+                          <option value="125mmHg">125mmHg</option>
+                          <option value="135mmHg">135mmHg</option>
+                          <option value="155mmHg">155mmHg</option>
+                        </select>
+
+                        <select className=" p-4 bg-transparent text-[#BFBFBF]  rounded focus:outline-none focus:shadow-outline border-1 border-[#BFBFBF] focus:ring-0">
+                          <option value="Pre-pandial">Pre-pandial</option>
+                          <option value="Post-pandial">Post-prandial</option>
+                        </select>
+                      </div>
+                      <div className="grid grid-cols-3 justify-between gap-4 w-full">
+                        <label>Age</label>
+                        <label>Weight (kg) </label>
+                        <label>Height (cm)</label>
+                        <select
+                          placeholder="Blood Type"
+                          className=" p-4 bg-transparent  text-[#BFBFBF]  rounded focus:outline-none focus:shadow-outline border-1 border-[#BFBFBF] focus:ring-0"
+                        >
+                          <option value="">Age</option>
+                          <option value="60">60</option>
+                          <option value="">90</option>
+                        </select>
+
+                        <select className=" p-4 bg-transparent text-[#BFBFBF]  rounded focus:outline-none focus:shadow-outline border-1 border-[#BFBFBF] focus:ring-0">
+                          <option value="Pre-pandial">Weight</option>
+                          <option value="Post-pandial">200</option>
+                        </select>
+                        <select className=" p-4  bg-transparent text-[#BFBFBF]  rounded focus:outline-none focus:shadow-outline border-1 border-[#BFBFBF] focus:ring-0">
+                          <option value="">176</option>
+                          <option value="">200</option>
+                        </select>
+                      </div>
+
                       <button
                         type="submit"
-                        className="text-white px-4 transform sm:uppercase text-md bg-[#F00530] hover:bg-red-800 focus:ring-4 focus:outline-none leading-loose focus:ring-red-300 font-medium rounded-[4px]  w-full py-2 lg:py-2 text-center"
+                        className="text-white mt-6 px-4 transform sm:uppercase text-md bg-[#F00530] hover:bg-red-800 focus:ring-4 focus:outline-none leading-loose focus:ring-red-300 font-medium rounded-[4px]  w-full py-2 lg:py-2 text-center"
                       >
                         Submit
                       </button>
