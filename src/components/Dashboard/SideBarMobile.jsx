6959d692fb37e73b6f02b955efb3c34311acd3ef
taofeek-ob <taofeek01@yahoo.com>
2022-08-31 08:04:14 +0100

    Implemented the auto-close function on dashboard sidebar

--- src/components/Dashboard/SideBarMobile.jsx
+++ src/components/Dashboard/SideBarMobile.jsx
@@ -1,10 +1,10 @@
 import React, { Fragment } from "react";
 import { Popover, Transition } from "@headlessui/react";
 import { XIcon } from "@heroicons/react/outline";
-import {LogoDark} from "../../assets/images"
+import { LogoDark } from "../../assets/images";
 import { Link, NavLink } from "react-router-dom";
 import { RiHistoryLine } from "react-icons/ri";
-import { MdOutlineSpaceDashboard } from 'react-icons/md';
+import { MdOutlineSpaceDashboard } from "react-icons/md";
 import { BsWallet } from "react-icons/bs";
 import { TbHeartbeat } from "react-icons/tb";
 
@@ -23,69 +23,93 @@ const SideBarMobile = () => {
         focus
         className="absolute top-0 right-0 w-[75%] sm:w-[50%] h-[100vh] transition overflow-x-clip z-20 lg:hidden"
       >
-        <div className="bg-black fixed inset bg-opacity-30 "/>
+        <div className="bg-black fixed inset bg-opacity-30 " />
         <div className="flex flex-col items-start gap-4 h-full bg-slate-50">
-        <div className="flex items-center justify-between px-5 w-full py-4 ">
-              <div>
-                <Link to="/dashboard" className="focus:outline-none focus:ring-0">
-                  <img
-                    className="h-8 w-auto"
-                    src={LogoDark}
-                    alt="Logo"
-                  />
-                </Link>
-              </div>
-              <div className="-mr-2">
-                <Popover.Button className="bg-ld rounded-md p-2 inline-flex items-center justify-center text-gray-800 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-red-500">
-                  <span className="sr-only">Close menu</span>
-                  <XIcon className="h-6 w-6" aria-hidden="true" />
-                </Popover.Button>
-              </div>
+          <div className="flex items-center justify-between px-5 w-full py-4 ">
+            <div>
+              <Link to="/dashboard" className="focus:outline-none focus:ring-0">
+                <img className="h-8 w-auto" src={LogoDark} alt="Logo" />
+              </Link>
             </div>
+            <div className="-mr-2">
+              <Popover.Button className="bg-ld rounded-md p-2 inline-flex items-center justify-center text-gray-800 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-red-500">
+                <span className="sr-only">Close menu</span>
+                <XIcon className="h-6 w-6" aria-hidden="true" />
+              </Popover.Button>
+            </div>
+          </div>
 
-        <div className="flex flex-col items-start gap-y-6 w-full">
-          <NavLink to="/dashboard/main" className={({ isActive }) =>
-            "flex items-center gap-x-3 w-full trans " + (isActive ? "bg-[#F00530] bg-opacity-10 text-[#F00530] before-sidebar" : "text-[#333333] bg-transparent")
-          }
-          >
-            <div className="flex items-center gap-2 w-full h-full py-3 px-2">
-              <MdOutlineSpaceDashboard className="h-6 w-6 ml-2" />
-              <p>Dashboard</p>
-            </div>
-          </NavLink>
-          <NavLink to="/dashboard/medical" className={({ isActive }) =>
-            "flex items-center gap-x-3 w-full trans " + (isActive ? "bg-[#F00530] bg-opacity-10 text-[#F00530] before-sidebar" : "text-[#333333] bg-transparent")
-          }
-          >
-            <div className="flex items-center gap-2 w-full py-3 px-2">
-              <TbHeartbeat className="h-6 w-6 ml-2" />
-              <p>Medicals</p>
-            </div>
-          </NavLink>
-          <NavLink to="/dashboard/wallet" className={({ isActive }) =>
-            "flex items-center gap-x-3 w-full trans " + (isActive ? "bg-[#F00530] bg-opacity-10 text-[#F00530] before-sidebar" : "text-[#333333] bg-transparent")
-          }
-          >
-            <div className="flex items-center gap-2 w-full py-3 px-2">
-              <BsWallet className="h-6 w-6 ml-2" />
-              <p>Wallet</p>
-            </div>
-          </NavLink>
-          <NavLink to="/dashboard/history"
-            className={({ isActive }) =>
-              "flex items-center gap-x-3 w-full trans " + (isActive ? "bg-[#F00530] bg-opacity-10 text-[#F00530] before-sidebar" : "text-[#333333] bg-transparent")
-            }
-          >
-            <div className="flex items-center gap-2 w-full py-3 px-2">
-              <RiHistoryLine className="h-6 w-6 ml-2" />
-              <p>History</p>
-            </div>
-          </NavLink>
+          <div className="flex flex-col items-start gap-y-6 w-full">
+            <NavLink
+              to="/dashboard/main"
+              className={({ isActive }) =>
+                "flex items-center gap-x-3 w-full trans " +
+                (isActive
+                  ? "bg-[#F00530] bg-opacity-10 text-[#F00530] before-sidebar"
+                  : "text-[#333333] bg-transparent")
+              }
+            >
+              {" "}
+              <Popover.Button>
+                <div className="flex items-center gap-2 w-full h-full py-3 px-2">
+                  <MdOutlineSpaceDashboard className="h-6 w-6 ml-2" />
+                  <p>Dashboard</p>
+                </div>
+              </Popover.Button>
+            </NavLink>
+            <NavLink
+              to="/dashboard/medical"
+              className={({ isActive }) =>
+                "flex items-center gap-x-3 w-full trans " +
+                (isActive
+                  ? "bg-[#F00530] bg-opacity-10 text-[#F00530] before-sidebar"
+                  : "text-[#333333] bg-transparent")
+              }
+            >
+              <Popover.Button>
+                <div className="flex items-center gap-2 w-full py-3 px-2">
+                  <TbHeartbeat className="h-6 w-6 ml-2" />
+                  <p>Medicals</p>
+                </div>
+              </Popover.Button>
+            </NavLink>
+            <NavLink
+              to="/dashboard/wallet"
+              className={({ isActive }) =>
+                "flex items-center gap-x-3 w-full trans " +
+                (isActive
+                  ? "bg-[#F00530] bg-opacity-10 text-[#F00530] before-sidebar"
+                  : "text-[#333333] bg-transparent")
+              }
+            >
+              <Popover.Button>
+                <div className="flex items-center gap-2 w-full py-3 px-2">
+                  <BsWallet className="h-6 w-6 ml-2" />
+                  <p>Wallet</p>
+                </div>{" "}
+              </Popover.Button>
+            </NavLink>
+            <NavLink
+              to="/dashboard/history"
+              className={({ isActive }) =>
+                "flex items-center gap-x-3 w-full trans " +
+                (isActive
+                  ? "bg-[#F00530] bg-opacity-10 text-[#F00530] before-sidebar"
+                  : "text-[#333333] bg-transparent")
+              }
+            >
+              <Popover.Button>
+                <div className="flex items-center gap-2 w-full py-3 px-2">
+                  <RiHistoryLine className="h-6 w-6 ml-2" />
+                  <p>History</p>
+                </div>
+              </Popover.Button>
+            </NavLink>
+          </div>
         </div>
-      </div>
       </Popover.Panel>
     </Transition>
-  )
-}
+  );
+};
 
-export default SideBarMobile
+export default SideBarMobile;
